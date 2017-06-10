de38585afa673430ed538985d46c2d422464ad1d
Cam Woloshyn <camw@nulogy.com>
2017-06-10 10:32:03 -0400

    Extracts #action method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -12,12 +12,12 @@ class Bottles
       when 0
         "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
         "#{quantity(number)} #{container(number)} of beer.\n" +
-        "Go to the store and buy some more, " +
+        "#{action(number)}" +
         "99 #{container(number)} of beer on the wall.\n"
       else
         "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
         "#{number} #{container(number)} of beer.\n" +
-        "Take #{pronoun(number)} down and pass it around, " +
+        "#{action(number)}" +
         "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
@@ -38,11 +38,19 @@ class Bottles
     end
   end
 
-  def quantity(number = :FIXME)
+  def quantity(number)
     if number == 0
       "no more"
     else
       number.to_s
+    end
+  end
+
+  def action(number = 0)
+    if number == 0
+      "Go to the store and buy some more, "
+    else
+      "Take #{pronoun(number)} down and pass it around, "
     end
   end
 end
