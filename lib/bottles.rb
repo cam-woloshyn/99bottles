c5a0710418df4b0889889d306f8dbbfdf360aed2
Cam Woloshyn <camw@nulogy.com>
2017-06-11 12:32:47 -0400

    Pushes factory down into BottleNumber class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,30 +8,29 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = bottle_number_for(number)
-    next_bottle_number = bottle_number_for(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
+    next_bottle_number = BottleNumber.for(bottle_number.successor)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}" +
     "#{next_bottle_number} of beer on the wall.\n"
   end
-
-  def bottle_number_for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    else
-      BottleNumber
-    end.new(number)
-  end
 end
 
 class BottleNumber
-  attr_reader :number
+  def self.for(number)
+    case number
+      when 0
+        BottleNumber0
+      when 1
+        BottleNumber1
+      else
+        BottleNumber
+    end.new(number)
+  end
 
+  attr_reader :number
   def initialize(number)
     @number = number
   end
